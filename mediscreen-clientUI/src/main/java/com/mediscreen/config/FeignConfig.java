a3d74c9be49c38e3a72f06d2f37ad6b24d34b382
bende47 <bendejustin@gmail.com>
2020-06-19 10:56:59 +0200

    Revert "Securisation de Service avec Spring Security"
    This reverts commit de576a37e643e4aa8b1a5636eff2e1941413ecef.

--- mediscreen-clientUI/src/main/java/com/mediscreen/config/FeignConfig.java
+++ mediscreen-clientUI/src/main/java/com/mediscreen/config/FeignConfig.java
@@ -1,14 +1,8 @@
 package com.mediscreen.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import feign.auth.BasicAuthRequestInterceptor;
 
 @Configuration
 public class FeignConfig {	
-	@Bean
-    public BasicAuthRequestInterceptor mBasicAuthRequestInterceptor(){
-        return  new BasicAuthRequestInterceptor("root", "vas@2020");
-    }
+
 }
